e99d810f842b67be34761c7c7af6c9b4d3603186
Hasan Abbasov <abbasovhsn05@gmail.com>
2024-06-10 01:44:07 +0400

    Extension metod

---
+++ ArrayExtensionMethod/ArrayExtension/MyCustomResizeMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ArrayExtensionMethod.ArrayExtension;
+
+public static class MyCustomResizeMethod
+{
+    public static void ResizeAndAdd<T>(this T[] array, T[] newElements)
+    {
+        int originalLength = array.Length;
+
+        int newLength = originalLength + newElements.Length;
+
+        Array.Resize(ref array, newLength);
+
+        for (int i = 0; i < newLength; i++)
+        {
+            array[originalLength + i] = newElements[i];
+        }
+    }
+}
---
+++ ArrayExtensionMethod/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public static class ArrayExtensions
+{
+    public static T[] ResizeAndAdd<T>(this T[] array, params T[] newElements)
+    {
+        if (array == null)
+        {
+            throw new ArgumentNullException(nameof(array), "Array cannot be null");
+        }
+
+        if (newElements == null || newElements.Length == 0)
+        {
+            return array; 
+        }
+
+        int originalLength = array.Length;
+        int newLength = originalLength + newElements.Length;
+
+        Array.Resize(ref array, newLength);
+
+        for (int i = 0; i < newElements.Length; i++)
+        {
+            array[originalLength + i] = newElements[i];
+        }
+
+        return array;
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3 };
+
+        // Extension method çağrısı ile diziye elemanlar ekleniyor
+        numbers = numbers.ResizeAndAdd(4, 5);
+
+        // Sonucu ekrana yazdırma
+        Console.WriteLine(string.Join(", ", numbers)); // Output: 1, 2, 3, 4, 5
+    }
+
+}
